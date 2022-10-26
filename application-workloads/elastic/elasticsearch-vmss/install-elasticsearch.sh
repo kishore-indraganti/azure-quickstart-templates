552fd17d6ae3c68d3ea47aae497b4eb1b08b44aa
Kishore Indraganti <kishoreece13@gmail.com>
2022-10-26 13:54:06 -0400

    fixing file content

--- application-workloads/elastic/elasticsearch-vmss/install-elasticsearch.sh
+++ application-workloads/elastic/elasticsearch-vmss/install-elasticsearch.sh
@@ -107,7 +107,6 @@ install_es()
 {
     wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
     apt-get install apt-transport-https
-    https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
     echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
     apt-get update -y
     apt-get install --reinstall resolvconf
